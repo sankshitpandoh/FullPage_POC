e9c2dceba658a264b28377cfc7f966725374685a
sankshitpandoh <sankshit02pandoh.sp@gmail.com>
2020-04-15 20:15:33 +0530

    some features studied and implemented

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+new fullpage('#fullpage', {
+	//options here
+	autoScrolling:true,
+    scrollHorizontally: true,
+    sectionsColor: ['#30475e', '#f8e1f4', '#77D8D8', '#f2a365', "#efa8e4", "#222831"],
+    anchors:['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection', 'sixthSection'],
+    slidesNavigation: true,
+    slidesNavPosition: 'bottom',
+    navigation: true,
+    navigationPosition: 'left',
+    scrollOverflow: true
+});
+
+//methods
+// fullpage_api.setAllowScrolling(false);
